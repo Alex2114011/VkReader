08fdea5a2455d37c0f05bdfe91f69741f785d261
Pavel <pav.gre4ixin@yandex.ru>
2020-11-17 17:57:55 +0300

    исправил отступы

--- VkReader/UIComponents/FeedScreen/FeedViewController.swift
+++ VkReader/UIComponents/FeedScreen/FeedViewController.swift
@@ -7,25 +7,24 @@
 
 import UIKit
 
-
-    class FeedViewController: BaseController {
-       
-        var viewModel: FeedViewModel
-        
-        init(viewModel: FeedViewModel) {
-            self.viewModel = viewModel
-            super.init(nibName: String(describing: FeedViewController.self), bundle: nil)
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
-
-       
-        
+class FeedViewController: BaseController {
+   
+    var viewModel: FeedViewModel
+    
+    init(viewModel: FeedViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: String(describing: FeedViewController.self), bundle: nil)
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+   
+    
+}
--- VkReader/UIComponents/LoginScreen/LoginViewController.swift
+++ VkReader/UIComponents/LoginScreen/LoginViewController.swift
@@ -9,22 +9,21 @@ import UIKit
 
 class LoginViewController: BaseController {
     
-       
-        var viewModel: LoginViewModel
-        
-        init(viewModel: LoginViewModel) {
-            self.viewModel = viewModel
-            super.init(nibName: String(describing: LoginViewController.self), bundle: nil)
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
+    var viewModel: LoginViewModel
+    
+    init(viewModel: LoginViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: String(describing: LoginViewController.self), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     @IBAction func goToAuthorization(_ sender: UIButton) {
         guard let core = corePresentation else { return }
@@ -32,7 +31,4 @@ class LoginViewController: BaseController {
     }
     
         
-    }
-
-
-
+}
