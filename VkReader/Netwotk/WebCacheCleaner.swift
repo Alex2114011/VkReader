a1c62e29ecf0fa03078b9870193310bfeb679d2b
Pavel <pav.gre4ixin@yandex.ru>
2020-11-17 14:25:12 +0300

    clean cash

---
+++ VkReader/Netwotk/WebCacheCleaner.swift
@@ -0,0 +1,23 @@
+//
+//  WebCacheCleaner.swift
+//  VkReader
+//
+//  Created by p.grechikhin on 17.11.2020.
+//
+
+import Foundation
+import WebKit
+
+class WebCacheCleaner {
+    
+    static func clean() {
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            }
+        }
+    }
+    
+}
