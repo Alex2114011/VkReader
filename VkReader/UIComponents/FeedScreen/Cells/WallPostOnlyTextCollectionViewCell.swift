07013cd106f9d2be9dd8e249c1e08c3a7a795432
Alex <Alex2114011@gmail.com>
2020-12-03 14:42:17 +0300

    ячейка с текстом

--- VkReader/UIComponents/FeedScreen/Cells/WallPostOnlyTextCollectionViewCell.swift
+++ VkReader/UIComponents/FeedScreen/Cells/WallPostOnlyTextCollectionViewCell.swift
@@ -34,7 +34,7 @@ class WallPostOnlyTextCollectionViewCell: UICollectionViewCell, VKReaderAbstract
         }
         groupNameLabel.text = model.group.name
         let textHeigth = model.text.height(withConstrainedWidth: UIScreen.main.bounds.width - 20, font: UIFont.systemFont(ofSize: 15))
-        self.model?.change(height: textHeigth)
+        self.model?.change(height: textHeigth + 49)
     }
     
     func set(delegate: VKReaderAbstractCellDelegate) {
